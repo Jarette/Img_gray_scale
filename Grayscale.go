92f47848810cc30aa1d55699b5d872f4de2cc6a3
Jarette Greene <jarettegreene09@gmail.com>
2023-11-02 23:55:20 -0500

    commit

--- Grayscale.go
+++ Grayscale.go
@@ -1,10 +1,36 @@
+/*****************************************************************************
+*                    
+*  Author:           Jarette Greene
+*  Email:            jkgreene0406@my.msutexas.edu / jarettegreene09@gmail.com
+*  Label:            P03
+*  Title:			 Image Ascii Art           
+*  Course:           CMPS 4143
+*  Semester:         Fall 2023
+* 
+*  Description:
+*	
+*		This Package converts an image to graycale and saves it to a new image  
+*
+* 
+*  Usage:
+*    - import to main.go file 
+* 
+*  Files           
+*       N/A 
+*****************************************************************************/
 package Img_gray_scale
 
+//necessary packages 
 import (
+	//fmt : allows for input and output to and from the console
 	"fmt"
+
+	//image: allows for decoding and converting of images to different formats
 	"image"
 	"image/color"
 	"image/png"
+
+	//os : allows to check for errors when opening a file 
 	"os"
 )
 
@@ -55,7 +81,7 @@ func Grayscale() {
 	}
 	defer grayFile.Close()
 	png.Encode(grayFile, grayImg)
-
+	// image to indicate the new image is created 
 	fmt.Println("Grayscale image saved.")
 
 }
